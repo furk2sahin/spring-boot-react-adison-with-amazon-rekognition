beb2c2b443d440cf7d245ccdc39d4c37e535e235
furk2sahin <furkannsahin.7@gmail.com>
2021-04-14 16:07:46 +0300

    Initial commit

--- API/src/main/java/com/furkannsahin/adison/repository/AdRepository.java
+++
@@ -1,9 +0,0 @@
-package com.furkannsahin.adison.repository;
-
-import com.furkannsahin.adison.model.Ad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface  AdRepository extends JpaRepository<Ad, Long> {
-}
